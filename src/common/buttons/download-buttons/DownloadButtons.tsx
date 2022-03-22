a581a745e4fad6e9e21d4cf42e3ebd64332bc599
SnezhanaKorsak <snezhana_korsak@mail.ru>
2022-03-22 11:48:19 +0300

    changed link for resume

--- src/common/buttons/download-buttons/DownloadButtons.tsx
+++ src/common/buttons/download-buttons/DownloadButtons.tsx
@@ -6,7 +6,7 @@ export const DownloadButtons = () => {
 
     return (
         <>
-            <a href="https://drive.google.com/file/d/1Ss78dfqwx_PCViioqCiyaEqXViyPcMFj/view?usp=sharing"
+            <a href="https://drive.google.com/file/d/1FbgSkzbsp4oo0FtVadphNdsHjSukprkF/view"
                className={style.btnFlip} data-back="➦" data-front="Download CV" target="_blank"/>
         </>
     );
